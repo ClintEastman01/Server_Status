8aa5185d6f08ac4c9f651b5da8112c948d0a135b
clint <clint4life@gmail.com>
2023-11-10 17:32:30 -0500

    restructured code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import urllib3
 import credentials
 import time
 import datetime
@@ -13,26 +12,26 @@ time_now = x.strftime("%d" " %b")
 
 
 def checkserver():
-    global SERVER_DOWN
-    # Checks to see if you have an internet connection
-    url = 'https://www.lugetech.com/'
-    try:
-        print('Checking server status')
-        response = urllib3.request("GET", url)
-        response.close()
-        if SERVER_DOWN:
-            SERVER_DOWN = False
-            message = "Alert! Your server is back up on " + time_now
-            requests.get(
-                'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + GROUP_CHAT_ID + '&text=' + message)
-    except Exception as e:
-        if not SERVER_DOWN:
-            SERVER_DOWN = True
-            message = "Alert! Your server is down on " + time_now
-            requests.get(
-                'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + GROUP_CHAT_ID + '&text=' + message)
+    while True:
+        global SERVER_DOWN
+        # Checks to see if you have an internet connection
+        url = 'http://localhost:8080/'
+        try:
+            response = requests.get(url)
+            response.close()
+            if SERVER_DOWN:
+                SERVER_DOWN = False
+                message = "Alert! Your server is back up on " + time_now
+                requests.get(
+                    'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + GROUP_CHAT_ID + '&text=' + message)
+        except Exception as e:
+            if not SERVER_DOWN:
+                print(e)
+                SERVER_DOWN = True
+                message = "Alert! Your server is down on " + time_now
+                requests.get(
+                    'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + GROUP_CHAT_ID + '&text=' + message)
+        time.sleep(3600)
 
 
-while True:
-    checkserver()
-    time.sleep(3600)
+checkserver()
